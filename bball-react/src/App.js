e09aa156de686b024c17012e73e26b81aa3c9eeb
justin_zhang@brown.edu <justinzhang@Justins-MacBook-Pro-5.local>
2018-07-12 15:22:02 -0400

    clean up react front end

--- bball-react/src/App.js
+++ bball-react/src/App.js
@@ -18,12 +18,13 @@ import 'react-table/react-table.css';
 
 
 class Banner extends Component{
+  // <img src={bball} alt="" className="bball"/>
   render() {
     return (
       <div className="navbar navbar-default">
-      <img src={bball} alt="" className="bball"/>
+      
       <div> Basketball Manager </div>
-      <img src={bball} alt="" className="bball"/>
+      
       </div>
     );
   }
@@ -39,7 +40,7 @@ class App extends Component {
       <div className="wrapper">
          <Banner />
          <div className="container">
-            <Sidebar />
+            <Sidebar gameData={this.gameData}/>
             <Content gameData={this.gameData}/>
          </div>
        </div>
@@ -52,20 +53,19 @@ class App extends Component {
 class Sidebar extends Component{
   render(){
     return(
-      <ul className="sidebar-nav">
-          <li>
-          <a href="#">Dashboard</a>
-          </li>
-          <li>
-          <a href="#">Shortcuts</a>
-          </li>
-          <li>
-          <a href="#">Overview</a>
-          </li>
-          <li>
-          <a href="#">Events</a>
-          </li>
-      </ul>
+      <div className="sidebar-container">
+            <nav className="nav-sidebar">
+                <ul className="nav">
+                    <li><a href="javascript:;">Home</a></li>
+                    <li><a href="javascript:;">Roster</a></li>
+                    <li><a href="javascript:;">Standings</a></li>
+                    <li><a href="javascript:;">Scouting</a></li>
+                    <li className="nav-divider"></li>
+                    <li><a href="javascript:;">Settings</a></li>
+                    <li><a href="javascript:;"><i className="glyphicon glyphicon-off"></i>Log Out</a></li>
+                </ul>
+            </nav>
+        </div>
     );
   }
 }
@@ -73,41 +73,81 @@ class Sidebar extends Component{
 class Content extends Component{
   constructor(props) {
     super(props);
-    this.state = {
-      title: 'Welcome to Basketball Manager',
-      content: 'Are you up for the challenge?',
-      buttonMsg: 'Start Game',
-    };
+    
 
-    this.handleClick = this.handleClick.bind(this);
+    this.handleNextPage = this.handleNextPage.bind(this);
     this.handleCheckBox = this.handleCheckBox.bind(this);
     this.handleRecruitSubmit = this.handleRecruitSubmit.bind(this);
-
-    this.nextPage = 'player';
 
     this.recruits = [];
     this.activeRecruits = [];
 
+    this.seasonData = {
+      temp: new Array(),
+      data: 0,
+      numDays : 0,
+      halfSize : 0,
+      teamsSize : 0
+    }
+
+    this.state = {
+      title: 'Welcome to Basketball Manager',
+      content: 'Are you up for the challenge?',
+      button: <button type="button" className="btn btn-outline-secondary" value = "player" onClick={this.handleNextPage}>Begin</button>
+    };
+
   }
 
-  handleClick(e) {
-    e.preventDefault();
-    console.log(e);
+  startSeason(){
+    let teams = this.props.gameData.teams;
+    this.seasonData.temp = new Array();
+    this.seasonData.temp.push.apply(this.seasonData.temp,teams);
+    this.seasonData.temp.splice(0,1);
+    this.numDays = (teams.length - 1); // Days needed to complete tournament
+    this.halfSize = teams.length / 2;
+    this.teamsSize = this.seasonData.temp.length;
+  }
 
-    if(this.nextPage == 'player'){
-      this.nextPage = 'recruit';
+  // playGames(){
+
+
+  //   var teamIdx = day % teamsSize;
+  //   scores += "<p>" + playGame(temp[teamIdx],teams[0]) + "<p>";
+
+  //   for (var idx = 1; idx < halfSize; idx++)
+  //   {
+  //     var firstTeam = temp[(day + idx) % teamsSize];
+  //     var secondTeam = temp[(day  + teamsSize - idx) % teamsSize];
+  //          playGame(firstTeam, secondTeam);
+  //   }
+  //   day++;
+
+  //     $(".scores").html(scores);
+  //     $(".standings").html(generateStandings());
+  //     if(numDays == day){
+  //         $(".page-continue").html(`<button onclick='initPlayoffs()'>Begin Playoffs</button>`);
+  //         //$(".page-continue").html(`<button onclick='endSeason()'>Season Done</button>`);
+  //     }
+  //   }
+
+  handleNextPage(e) {
+    e.preventDefault();
+    console.log(e.target.value);
+
+    if(e.target.value == 'player'){
       this.setState(prevState => ({
         title: 'Your Roster',
-        content: <PlayerTable data={this.props.gameData.myTeam.players}/>,
-        buttonMsg: 'Start Recruiting'
+        content: <div><PlayerTable data={this.props.gameData.myTeam.players}/><PlayerTable data={this.props.gameData.myTeam.players}/><PlayerTable data={this.props.gameData.myTeam.players}/></div> ,
+        button: <button type="button" className="btn btn-outline-secondary" value = "recruit" onClick={this.handleNextPage}>Recruit!</button>
       }));
     }
-    else if(this.nextPage == 'recruit'){
+    else if(e.target.value == 'recruit'){
       this.recruits =generateRecruits(); 
       this.setState(prevState => ({
         title: 'This Year Recruits',
         content: <RecruitTable data={this.recruits} handleCheckBox={this.handleCheckBox} 
-        handleRecruitSubmit={this.handleRecruitSubmit}/>
+        handleRecruitSubmit={this.handleRecruitSubmit}/>,
+        button: ""
       }));
     }
   }
@@ -135,7 +175,6 @@ class Content extends Component{
     console.log(e.target);
     console.log(this.recruits);
     for(let recruit of this.recruits){
-      
       if(this.activeRecruits.includes(recruit.name)){
         this.props.gameData.myTeam.players.push(recruit);
       }
@@ -143,7 +182,7 @@ class Content extends Component{
     this.setState(prevState => ({
         title: 'Your Roster',
         content: <PlayerTable data={this.props.gameData.myTeam.players}/>,
-        buttonMsg: 'Start Recruiting'
+        button: 'Start Recruiting'
       }));
   }
 
@@ -152,7 +191,7 @@ class Content extends Component{
       <div className ="content">
         <div className="page-title">{this.state.title}</div>
         <div className="page-content">{this.state.content}</div>
-        <div className="page-continue"><button type="button" className="btn btn-outline-secondary" onClick={this.handleClick}>{this.state.buttonMsg}</button></div>
+        <div className="page-continue">{this.state.button}</div>
       </div>
     );
   }
