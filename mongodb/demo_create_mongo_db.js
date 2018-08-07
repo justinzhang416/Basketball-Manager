93cb0284874df19de56cfc8011cb52088cd57de5
justin_zhang@brown.edu <justinzhang@Justins-MacBook-Pro-5.local>
2018-08-07 09:42:56 -0400

    play with db

--- mongodb/demo_create_mongo_db.js
+++ mongodb/demo_create_mongo_db.js
@@ -1,8 +1,12 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb://localhost:5000/mydb";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
+
+MongoClient.connect(url, {useNewUrlParser: true } ).then(() => {
+console.log('mongoDB is connected...')
+})
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   console.log("Database created!");
+//   db.close();
+// });
