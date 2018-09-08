1ffa969f809b33f9bc7d13f8a3f596e6eb53ae3f
justin_zhang@brown.edu <justinzhang@Justins-MacBook-Pro-5.local>
2018-09-08 16:43:39 -0400

    build

--- src/server.js
+++ src/server.js
@@ -61,9 +61,18 @@ client.connect();
 //   client.end();
 // });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  });
+}
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
